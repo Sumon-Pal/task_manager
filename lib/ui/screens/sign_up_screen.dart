9d838c342c377b7a95fb2a7fcb5ed80098b4e494
Sumon-Pal <sumon.pal.ice@gmail.com>
2025-07-04 12:43:35 +0600

    Sign Up Screen Ui Design complete

---
+++ lib/ui/screens/sign_up_screen.dart
@@ -0,0 +1,167 @@
+import 'package:email_validator/email_validator.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:task_manager/ui/screens/sign_in_screen.dart';
+import 'package:task_manager/ui/utils/screen_background.dart';
+
+class SignUpScreen extends StatefulWidget {
+  static final String name = '/sign-up';
+  const SignUpScreen({super.key});
+
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  final TextEditingController _emailTEcontroller = TextEditingController();
+  final TextEditingController _firstNameTEcontroller = TextEditingController();
+  final TextEditingController _lastNameTEcontroller = TextEditingController();
+  final TextEditingController _mobileTEcontroller = TextEditingController();
+  final TextEditingController _passwordTEcontroller = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ScreenBackground(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              key: _formKey,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              child: Column(
+                //mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 150,),
+                  Text(
+                    'Join With Us',
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  const SizedBox(height: 42),
+                  TextFormField(
+                    controller: _emailTEcontroller,
+                    textInputAction: TextInputAction.next,
+                    decoration: InputDecoration(hintText: 'Email'),
+                    validator: (String? value){
+                      String email = value ?? '';
+                      if(EmailValidator.validate(email)==false){
+                        return 'Enter a valid email';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 14),
+                  TextFormField(
+                    controller: _firstNameTEcontroller,
+                    textInputAction: TextInputAction.next,
+                    decoration: InputDecoration(hintText: 'First Name'),
+                    validator: (String? value){
+                      if((value?.length ?? 0)<=6){
+                        return 'Enter a valid password';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  TextFormField(
+                    controller: _lastNameTEcontroller,
+                    textInputAction: TextInputAction.next,
+                    decoration: InputDecoration(hintText: 'Last Name'),
+                    validator: (String? value){
+                      if((value?.length ?? 0)<=6){
+                        return 'Enter a valid password';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  TextFormField(
+                    controller: _mobileTEcontroller,
+                    keyboardType: TextInputType.number,
+                    textInputAction: TextInputAction.next,
+                    decoration: InputDecoration(hintText: 'Mobile'),
+                    validator: (String? value){
+                      if((value?.length ?? 0)<=6){
+                        return 'Enter a valid password';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  TextFormField(
+                    controller: _passwordTEcontroller,
+                    obscureText: true,
+                    textInputAction: TextInputAction.done,
+                    decoration: InputDecoration(hintText: 'PassWord'),
+                    validator: (String? value){
+                      if((value?.length ?? 0)<=6){
+                        return 'Enter a valid password';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: _onTapSignUpButton,
+                    child: Icon(Icons.arrow_circle_right_outlined),
+                  ),
+                  const SizedBox(height: 32),
+                  Center(
+                    child: Column(
+                      children: [
+                        RichText(
+                          text: TextSpan(
+                            text: "Have account?",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black,
+                              letterSpacing: 0.4,
+                            ),
+                            children: [
+                              TextSpan(
+                                text: "Sign In",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                  color: Colors.green,
+                                  letterSpacing: 0.4,
+                                ),
+                                recognizer: TapGestureRecognizer()
+                                  ..onTap = _onTapSignInButton,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+  void _onTapSignUpButton() {
+    if(_formKey.currentState!.validate()){
+      //TODO: Sign in with API
+    }
+  }
+
+  void _onTapForgotPasswordButton() {}
+
+  void _onTapSignInButton() {
+    Navigator.popAndPushNamed(context,SignInScreen.name);
+  }
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _emailTEcontroller.dispose();
+    _firstNameTEcontroller.dispose();
+    _lastNameTEcontroller.dispose();
+    _mobileTEcontroller.dispose();
+    _passwordTEcontroller.dispose();
+    super.dispose();
+  }
+}
